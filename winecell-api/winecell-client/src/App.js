b614a0f8872121104b8ce34efa785b454fa3609d
cmlugoce <cmlugoce@syr.edu>
2018-09-12 16:23:39 -0700

    add class constructor

--- winecell-api/winecell-client/src/App.js
+++ winecell-api/winecell-client/src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Wines from './containers/Wines';
 import './App.css';
 
+
+
 class App extends Component {
+
+ constructor(props) {
+   super(props)
+
+   this.state = {
+     wines: []
+   }
+ }
+
+  componentDidMount() {
+     fetch('http://localhost:3001/api/wines')
+      .then(response => response.json())
+      .then(wines => this.setState({wines}))
+  }
   render() {
+
+   
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+         
+          <h1 className="App-title">WINECELL</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <Wines wines={this.state.wines} />
       </div>
+      
     );
   }
 }
