667cdf9e32634e518d5f315952ee12eddbe61b47
cmlugoce <cmlugoce@syr.edu>
2018-09-12 18:20:17 -0700

    remove const and add class

--- winecell-api/winecell-client/src/containers/Wines.js
+++ winecell-api/winecell-client/src/containers/Wines.js
@@ -1,23 +1,22 @@
-import React from 'react'
+import React, {Component} from 'react'
+import WineCard from "../components/WineCard";
+import WineForm from "../containers/WineForm";
 import './Wines.css';
 
+  class Wines extends Component {
+      render() {
+       return (
+         <div className="WinesContainer">
+       <h1> Wines </h1>
+       {this.props.wines.map(wine => <WineCard key={wine.id} wine={wine} />)}     
+        <WineForm />
+       </div>
+    );  
 
-  
-const Wines = (props)=> (
-<div className="WinesContainer">
-  <h1> Wines </h1>
-  {props.wines.map(wine =>
-  <div key={wine.id} className= "WineCard">
-   <h3>{wine.name}</h3>
-   <p> Description: {wine.description} </p>
-   <p> Wine type: {wine.wine_type} </p>
-   <p> Year: {wine.year} </p>
-   <img className="WineImage" src={wine.image} alt={wine.name} />
-   <p> Price:  ${wine.price} </p>
-   </div>
-  )}
+ }
+}
 
-  </div>
-);
+
+
 
 export default Wines;
