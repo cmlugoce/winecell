ef654365ec9593cfe25bd436c3cb7b74f46161d8
cmlugoce <cmlugoce@syr.edu>
2018-09-12 18:18:53 -0700

    add form

---
+++ winecell-api/winecell-client/src/containers/WineForm.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class WineForm extends Component {
+
+  render() {
+
+    return (
+
+       <div>
+           Add a wine bottle 
+           <form>
+
+            </form>
+        </div>
+
+    )
+
+
+
+
+  }
+
+
+
+}
+
+export default WineForm;
