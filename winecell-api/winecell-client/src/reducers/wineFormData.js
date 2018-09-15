cd1115d9fc521d0d02fe1c10fed16c52a575fc3e
cmlugoce <cmlugoce@syr.edu>
2018-09-15 14:25:54 -0700

    add initialState const

--- winecell-api/winecell-client/src/reducers/wineFormData.js
+++ winecell-api/winecell-client/src/reducers/wineFormData.js
@@ -1,15 +1,21 @@
-export default (state = {
-    name: '',
-    description: '',
-    wine_type: '',
-    image: '',
-    year: 0,
-    price: 0
-}, action) => {
+const initialState = {
+name: '',
+description: '',
+wine_type: '',
+image: '',
+year: 0,
+price: 0
+}
+
+export default (state = initialState,
+     action) => {
 
     switch(action.type) {
         case 'UPDATED_DATA':
-        return action.wineFormData
+        return action.wineFormData;
+
+        case 'RESET_WINE_FORM':
+        return initialState;
 
         default:
         return state;
