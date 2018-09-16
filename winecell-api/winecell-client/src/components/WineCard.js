956eab693b68be5e91645fc996aded02d9d9d3ed
cmlugoce <cmlugoce@syr.edu>
2018-09-16 14:19:30 -0700

    remove jsx additional elements

--- winecell-api/winecell-client/src/components/WineCard.js
+++ winecell-api/winecell-client/src/components/WineCard.js
@@ -4,13 +4,12 @@ const WineCard = ({wine}) => (
     <div key={wine.id} className="WineCard">
    
     <h2 id="wineName">{wine.name}</h2>
-   <p> Description: {wine.description} </p>
-   <p> Wine type: {wine.wine_type} </p>
-   <p> Year: {wine.year} </p>
-   <img className="WineImage" src={wine.image} alt={wine.name} />
-   <p> Price:  ${wine.price} </p>
-  
- 
+    <img className="WineImage" src={wine.image} alt={wine.name} />
+    <br />
+    <br />
+    <h3> Wine type: {wine.wine_type} </h3>
+
+    <br />
    <Link to={{pathname: `/wines/${wine.id}`, state: {wine: wine.id}}}>View Wine</Link>
    </div>
 
