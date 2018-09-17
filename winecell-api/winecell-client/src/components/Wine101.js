430823344964f29d6cb227b366786d8c95b4a7f7
cmlugoce <cmlugoce@syr.edu>
2018-09-17 08:58:36 -0700

    add wine 101 info

---
+++ winecell-api/winecell-client/src/components/Wine101.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Container, Header, Image} from 'semantic-ui-react'
+
+export const Wine101 = () => {
+    return (
+     <Container>
+         <br />
+         <br />
+         <br />
+         <br />
+         <br />
+         <br />
+         <div className='container' id='wine101'>
+      <Header as='h1' > WINE 101: Common Types of Wine </Header>
+      <center>
+      <Image src="https://winefolly-wpengine.netdna-ssl.com/wp-content/uploads/2015/05/common-types-of-wine-by-wine-folly.jpg" size='massive' />
+      </center>
+      <br />
+      <Header as='h2'> Cabernet Sauvignon </Header>
+      <br />
+
+      <h3>
+              <strong>Taste:</strong>    Black Cherry, Black Currant, Baking Spices and Cedar (from oak)
+            </h3>
+            <h3> <strong>Style:</strong> Full-bodied Red Wine</h3>
+            <h3><strong>Description:</strong> Cabernet Sauvignon is a full-bodied red grape first heavily planted in the Bordeaux region. Today, it’s the most popular wine variety in the world. Wines are full-bodied with bold tannins and a long persistent finish driven mostly by the higher levels of alcohol and tannin that often accompany these wines.</h3>
+
+          <Header as='h2'> Syrah </Header>
+          <h3>
+              <strong>Taste:</strong> Blueberry, plum, tobacco, meat, black pepper, violet
+            </h3>
+            <h3> <strong>Style:</strong> Full-bodied Red Wine</h3>
+            <h3><strong>Description:</strong> Syrah (a.k.a. Shiraz) is a full-bodied red wine that’s heavily planted in the Rhône Valley in France and Australia. The wines have intense fruit flavors and middleweight tannins. Syrah is commonly blended with Grenache and Mourvèdre to create the red Rhône blend. The wine often has an aggressively meaty (beef broth, jerky) quality.</h3>
+            <Header as='h2'> Zinfandel </Header>
+            <h3>
+              <strong>Taste:</strong>  A broad, exotic array of fruits from stone (overripe nectarine), to red (raspberry, sour cherry), to blue (plum, blueberry), to black (blackberry, boysenberry), Asian 5 Spice Powder, Sweet Tobacco  
+            </h3>
+            <h3> <strong>Style:</strong>Medium-bodied to full-bodied Red Wine </h3>
+            <h3><strong>Description:</strong> Zinfandel (aka Primitivo) is a medium-bodied red wine that originated in Croatia. Wines are fruit-forward and spicy with a medium length finish. Zinfandel is a red grape that may be better known as the rosé wine White Zinfandel.
+            </h3>
+            <Header as='h2'> Pinot Noir </Header>
+            <h3>
+              <strong>Taste:</strong>  Very red fruited (cherry, cranberry) and red-floral (rose), often with appealing vegetal notes of beet, rhubarb, or mushroom  
+            </h3>
+            <h3> <strong>Style:</strong> Lighter-bodied Red Wine with higher acid and soft tannin</h3>
+            <h3><strong>Description:</strong> Pinot Noir is a dry light-bodied first widely planted in France. The wines always lead with higher acid and soft tannins.
+            </h3>
+            <Header as='h2'> Chardonnay </Header>
+            <h3>
+              <strong>Taste:</strong> Yellow citrus (Meyer lemon), yellow pomaceous fruits (yellow pear and apple), and tropical fruits (banana, pineapple), often cinnamon, butterscotch, and toasted caramel notes (from oak)   
+            </h3>
+            <h3> <strong>Style:</strong> Medium to full-bodied white wine</h3>
+            <h3><strong>Description:</strong>  Chardonnay is a dry full-bodied white wine that was planted in large quantities for the first time in France. When oak-aged, Chardonnay will have spicy, bourbon-y notes. Unoaked wines are lighter and zesty with apple and citrus flavors. Chardonnay is the white grape of Burgundy.
+            </h3>
+            <Header as='h2'> Sauvignon Blanc </Header>
+            <h3>
+              <strong>Taste:</strong> Aggressively-citrus-driven (grapefruit pith), with some exotic fruits (honeydew melon, passion fruit, kiwi) and always an herbaceous quality (grass, mint, green pepper)   
+            </h3>
+            <h3> <strong>Style:</strong> Light-bodied to medium-bodied white wine </h3>
+            <h3><strong>Description:</strong> Sauvignon Blanc is a dry white grape first widely planted in France. Wines are tart, typically with herbal green fruit flavors. Sauvignon Blanc is a parent grape of Cabernet Sauvignon.
+            </h3>
+            <Header as='h2'> Pinot Gris </Header>
+            <h3>
+              <strong>Taste:</strong>  Delicate citrus (lime water, orange zest)  and pomaceous fruits (apple skin, pear sauce), white floral notes, and cheese rind (from lees usage)  
+            </h3>
+            <h3> <strong>Style:</strong>Light-bodied White Wine </h3>
+            <h3><strong>Description:</strong> Pinot Gris is a dry light-bodied white grape that is planted heavily in Italy, but also in France and Germany. Wines are light to middle-weight and easy drinking, often with some bitter flavor on the palate (bitter almond, quinine)
+            </h3>
+            <Header as='h2'> Riesling </Header>
+            <h3>
+              <strong>Taste:</strong>   Citrus (kefir lime, lemon juice) and stone-fruit (white peach, nectarine) always feature prominently, although there are also usually floral and sweet herbal elements as well  
+            </h3>
+            <h3> <strong>Style:</strong>Floral and fruit-driven aromatic white that comes in variable sweetness. Some producers choose not to ferment all the grape sugar and therefore make the wine in an “off-dry” style. </h3>
+            <h3><strong>Description:</strong> Always very high in acid, when made as a table wine Rieslings can be harmoniously sweet (sweet and sour) or dry (very acidic). The wine is polarizing because some people find dry styles too acidic and sweet styles too cloying, but sweetness is always a wine making decision and not inherent to the grape.
+            </h3>
+     
+     
+     
+     
+     
+     
+      </div>
+</Container>
+    )
+}
+
+export default Wine101;
