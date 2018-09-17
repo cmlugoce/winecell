68686fc442600773c8eddfc7c5ef5f5aade6fcf6
cmlugoce <cmlugoce@syr.edu>
2018-09-17 08:58:11 -0700

    styling

--- winecell-api/winecell-client/src/components/Error.js
+++ winecell-api/winecell-client/src/components/Error.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Container, Image} from 'semantic-ui-react'
 
 
 const Error= () => {
 
     return (
-        <div>
-
-            <p>Error</p>
+        <Container>
+        <div className='error-page' >
+           <Image src="https://issues.jboss.org/secure/attachment/12387931/404-page-04.png?_sscc=t" size='massive' />
+            
 
         </div>
+        </Container>
     );
 };
 
