5ced032e7f0b2483c34ae3f65a7128458895b283
cmlugoce <cmlugoce@syr.edu>
2018-09-17 08:57:23 -0700

    add seed data

--- winecell-api/db/seeds.rb
+++ winecell-api/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Wine.create(name: "Juggernaut Hillside", description: "Full body, medium acidity and grippy tannins.  ", wine_type: "Cabernet Sauvignon", year: 2015, price: 20, image:"https://www.warehousewinesandspirits.com/images/sites/warehousewinesandspirits/labels/w1566740fw_1.jpg")
+Wine.create(name: "Cotes de Provence", description: "Peach nose and flavor. Nice round body with a bit of creaminess . ", wine_type: "Rose", year: 2017, price: 17, image:"https://www.savortonight.com/wp-content/uploads/2017/03/VieVite%CC%81-rose-wine-%E2%80%93-@fooddrunkie-6974.jpg")
+Wine.create(name: "The Snitch", description: "Creamy, oaky and still very fresh ", wine_type: "Chardonnay", year: 2016, price: 25, image:"https://cdn.shopify.com/s/files/1/1580/4729/products/Screen_Shot_2018-06-15_at_11.42.20_PM.png?v=1529131351")
+Wine.create(name: "Capellania", description: "Nice clean yellow ", wine_type: "Rioja", year: 2012, price: 24, image: "https://cdn.shopify.com/s/files/1/0124/8792/products/Marques_de_Murrieta_Capellania_1000x.jpg?v=1535260699")
+Wine.create(name: "Decima Aurea", description: "Very nice try and smooth not to Heavy and just perfect. ", wine_type: "Merlot", year: 2007, price: 40, image:"http://www.tenutasantamaria.wine/wp-content/uploads/2014/04/vini-03-decima.jpg")
+Wine.create(name: "The Boxer Shiraz", description: " Delicious mildly Oakey Shiraz ", wine_type: "Syrah", year: 2015, price: 30, image:"images.vivino.com/thumbs/_jEM-6XiQdOfFVLGXQLPDA_pl_375x500.png")
+Wine.create(name: "Terroir Los Miradores", description: " Dark deep purple colour ", wine_type: "Malbec", year: 2015, price: 31, image:"https://pbs.twimg.com/media/DAdbT6KWsAQbJBi.jpg")
+Wine.create(name: "Prosecco Superiore", description: " Pale yellow colour with decent bubbles. On the nose citrus, yeast and minerals.
+On the palate light bodied with balanced acidity and a long melon finish. ", wine_type: "Prosecco", year: 2015, price: 25, image:"https://altaneve.com/wp-content/uploads/2016/11/altaneve-z.jpg")
+Wine.create(name: "Electra Red", description: " Light grapey flavor with a nice nose. Smells like flowers and is smooth. Nice sipping wine. Would pair nicely with a heavy pastry. ", wine_type: "California Moscato", year: 2015, price: 20, image:"https://justintoxicate.files.wordpress.com/2013/10/20131001_172839.jpg")
+Wine.create(name: "Belle Glos", description: "Notes of vanilla oak and candle wax on the nose. Color is dark in the glass, really dark for Pinot. The taste is a heavy cherry, blueberries and dark mulberry compote. ", wine_type: "Pinot Noir", year: 2016, price: 40, image:"https://i.ytimg.com/vi/xCZqglSo5tg/hqdefault.jpg")
+
+
