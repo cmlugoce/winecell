d118e4b5d6ac24ce6a95938a0faa6d19b9d03163
cmlugoce <cmlugoce@syr.edu>
2018-09-25 11:27:23 -0700

    add sortingbyMaxLikes function

--- winecell-api/winecell-client/src/containers/Wines.js
+++ winecell-api/winecell-client/src/containers/Wines.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import WineCard from "../components/WineCard";
 import { getWines } from '../actions/wines';
+import {Button} from 'semantic-ui-react';
+
 import './Wines.css';
 
 
@@ -9,14 +11,42 @@ import './Wines.css';
 
   class Wines extends Component {
 
+    
+
+    state = {
+      
+     
+      wines: [],
+    }
+
     componentDidMount() {
      this.props.getWines()
   }
+  sortByMaxLikes = () => {
+    //alert('click')
+    const { wines} = this.props;
+    
+    const sortedWines = wines.sort(function(a, b) {
+        return b.likes - a.likes;
+        
+        
+      })
+   
+     this.setState({
+      wines: sortedWines.map(wine => <WineCard key={wine.id} wine={wine}  />)
+  })
+  }
+  
+  
       render() {
+       
        return (
          <div className="WinesContainer">
-       
-       {this.props.wines.map(wine => <WineCard key={wine.id} wine={wine}  />)}     
+        <br />
+        <Button inverted color='yellow' onClick={this.sortByMaxLikes} content='Sort by Max Likes' />  
+       <br />
+       {this.props.wines.map(wine => <WineCard key={wine.id} wine={wine}  />)}   
+     
        {this.props.children}
        </div>
     );  
