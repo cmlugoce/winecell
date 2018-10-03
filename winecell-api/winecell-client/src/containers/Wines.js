b30ff732086858772ca683f520e848a3e6301f20
cmlugoce <cmlugoce@syr.edu>
2018-10-03 10:55:35 -0700

    update sorting andd add sort by name

--- winecell-api/winecell-client/src/containers/Wines.js
+++ winecell-api/winecell-client/src/containers/Wines.js
@@ -3,53 +3,113 @@ import {connect} from 'react-redux';
 import WineCard from "../components/WineCard";
 import { getWines } from '../actions/wines';
 import {Button} from 'semantic-ui-react';
-
+import {Input} from 'mdbreact';
+//import Filters from './Filters';
+//import Filter from '../components/Filter';
+///import {Route, Switch } from 'react-router-dom';
 import './Wines.css';
 
 
 
+const winer=getWines()
 
   class Wines extends Component {
-
-    constructor(props) {
-      super(props)
-   
+    
+   constructor(props, context){
+     super(props, context)
+    this.state = {
+      search: '',
+      data: {},              
+      sortBy: 'name',
+      sortedBy: 'likes', 
+      
+         
+    }
   }
     
 
     componentDidMount() {
-     this.props.getWines()
+      this.props.getWines()
+      .then(wines => this.setState({ data: this.props.wines }));
+    
+    }
+    
+  
+  
+
+sortBy=(likes) => {
+ 
+  let wines = this.state.data;
+    wines.sort(this.compareBy(likes));
+    this.setState({sortedBy: wines});
   }
-  sortByMaxLikes = () => {
-    //alert('click')
-    //const  wines =this.props.wines;
-   
-   // console.log(wines)
-    const sortedWines = Object.assign({}, this.props.wines, 
-    this.props.wines.sort(function(a, b) {
-        return b.likes - a.likes;
+ 
+    
+    compareBy(likes) {
+      return function (a, b) {
+        if (a[likes] > b[likes]) return -1;
+        if (a[likes] < b[likes]) return 1;
+        return 0;
+      };
+    }
+    sortByName=(name) => {
+ 
+      let wines = this.state.data;
+        wines.sort(this.compareByName(name));
+        this.setState({sortBy: wines});
+      }
+     
         
-        
-      })
-    )
-   
-     this.setState({
-      wines: sortedWines
-  })
+        compareByName(name) {
+          return function (a, b) {
+            if (a[name] < b[name]) return -1;
+            if (a[name] > b[name]) return 1;
+            return 0;
+          };
+        }
+  
+
+handleChange= (event) =>{
+  this.setState({ search: event.target.value });
+
 }
-  
-  
+
+
   
       render() {
-       
+
+        
+        
+        const {search} = this.state;
+        const filteredWines = this.props.wines.filter( wine =>{
+            return wine.name.toLowerCase().indexOf( search.toLowerCase() ) !== -1
+        })
+      
+  
        return (
          <div className="WinesContainer">
         <br />
-        <Button inverted color='yellow' onClick={this.sortByMaxLikes} content='Sort by Max Likes' />  
+        
+        <div className="col">
+              <Input label="Filter by name" icon="search" onChange={this.handleChange}/>
+              
+
+         </div>         
+         <br />
+         <br />
+         <div>
+         <Button inverted color='yellow' onClick={() => this.sortBy('likes')}  content='Sort by Likes'/>  
+         <Button inverted color='yellow' onClick={() => this.sortByName('name')}  content='Sort by Name'/>  
+
        <br />
-       {this.props.wines.map(wine => <WineCard key={wine.id} wine={wine}  />)}   
+       {filteredWines.map(wine => <WineCard key={wine.id} wine={wine}  />)}  
+       
+       
+      </div>
      
-       {this.props.children}
+       
+      
+       
        </div>
     );  
 
