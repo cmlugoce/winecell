10c5d173ad4fc3f0be26a72930ec320acebc1c7f
cmlugoce <cmlugoce@syr.edu>
2018-10-03 18:55:31 -0700

    update sorting

--- winecell-api/winecell-client/src/containers/Wines.js
+++ winecell-api/winecell-client/src/containers/Wines.js
@@ -4,14 +4,11 @@ import WineCard from "../components/WineCard";
 import { getWines } from '../actions/wines';
 import {Button} from 'semantic-ui-react';
 import {Input} from 'mdbreact';
-//import Filters from './Filters';
-//import Filter from '../components/Filter';
-///import {Route, Switch } from 'react-router-dom';
 import './Wines.css';
 
 
 
-const winer=getWines()
+
 
   class Wines extends Component {
     
@@ -34,17 +31,13 @@ const winer=getWines()
     
     }
     
-  
-  
-
-sortBy=(key) => {
+    sortBy=(key) => {
  
-  let wines = this.state.data.wines;
-    wines.sort(this.compareBy(key));
-    this.setState({sortedBy: wines});
-  }
- 
-    
+      let wines = this.state.data.wines;
+      wines.sort(this.compareBy(key));
+      this.setState({sortedBy: wines});
+    }
+  
     compareBy(key) {
       return function (a, b) {
         if (a[key] > b[key]) return -1;
@@ -61,43 +54,40 @@ handleChange= (event) =>{
 }
 
 
-  
-      render() {
 
-                
-        
-        
+ 
+
+  
+      render() {             
+               
         const {search} = this.state;
         const filteredWines = this.props.wines.filter( wine =>{
             return wine.name.toLowerCase().indexOf( search.toLowerCase() ) !== -1
         })
       
-  
+      
        return (
          <div className="WinesContainer">
-        <br />
+          <br />
         
-        <div className="col">
-              <Input label="Filter by name" icon="search" onChange={this.handleChange}/>
+              <div className="col">
+                <Input label="Filter by name" icon="search" onChange={this.handleChange}/>
               
 
-         </div>         
-         <br />
-         <br />
-         <div>
-         <Button inverted color='yellow' onClick={() => this.sortBy('likes')}  content='Sort by Likes'/>  
-         <Button inverted color='yellow' onClick={() => this.sortBy('name')}  content='Sort by Name'/>  
+               </div>         
+                 <br />
+                 <br />
+                  <div>
+                     <Button inverted color='yellow' onClick={() => this.sortBy('likes')}  content='Sort by Likes'/>  
+                     <Button inverted color='yellow' onClick={() => this.sortBy('name')}  content='Sort by Name'/>  
+         
+                   <div>
+                      {filteredWines.map(wine => <WineCard key={wine.id} wine={wine}  />)}      
+                   </div> 
+                 <br />       
+              </div>   
             
-       <br />
-       {filteredWines.map(wine => <WineCard key={wine.id} wine={wine}  />)}  
-       
-       
-      </div>
-     
-       
-      
-       
-       </div>
+         </div>
     );  
 
  }
